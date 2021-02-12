664edf10c6aa33ace96fe7baa17877771d25b357
crafty35a <crafty35a@yahoo.com>
2021-02-12 12:50:05 -0500

    Update mullvad.sh

--- mullvad.sh
+++ mullvad.sh
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env bash
 
 # -e option instructs bash to immediately exit if any command [1] has a non-zero exit status
@@ -12,13 +13,14 @@ mullvadVpnInterfaceRegex="mullvad-\w*"
 wireguardConfigurationDirectory="/etc/wireguard/"
 connectedWireguardConfiguration=""
 
+# I don't use this, my interface will always be wg0
 # A method to retrieve the current connected Mullvad interface.
-checkMullvadConnectivity() {
-	# Check if Mullvad VPN is already connected.
-	connectedWireguardConfiguration=$(ip addr | grep --word-regexp "$1" | cut -d " " -f 2 | tr -d ":")
-	# Return an arbitrary integer value | This value is not checked right now
-	return 0
-}
+# checkMullvadConnectivity() {
+#         # Check if Mullvad VPN is already connected.
+#         connectedWireguardConfiguration=$(ip addr | grep --word-regexp "$1" | cut -d " " -f 2 | tr -d ":")
+#         # Return an arbitrary integer value | This value is not checked right now
+#         return 0
+# }
 
 #checkMullvadConnectivity "$mullvadVpnInterfaceRegex"
 
@@ -28,45 +30,43 @@ checkMullvadConnectivity() {
 # Extract the wireguard configuration list that is available in /etc/wireguard
 newWireguardConfigurationList=$(sudo ls $wireguardConfigurationDirectory | grep --word-regexp "$mullvadVpnInterfaceRegex")
 
-# Satisfies this condition if a connected interface was found.
-#if [[ -n "$connectedWireguardConfiguration" ]]; then
+printf "" # Blank space for formatting
+printf "Randomizing the Mullvad VPN Connection..."
 
-echo "" # Blank space for formatting
-echo "Cron is re-configuring the connected VPN."
-
-
-
-
-if test -f [ ! -f /etc/wireguard/wg0.conf ]
+#Check if wg0.conf exists
+if sudo [ ! -f /etc/wireguard/wg0.conf ]
 then
-	echo "wg0.conf doesn't exist yet (first time run)"
-	# Pick a wireguard interface dat random to connect to next
-	newWireguardConfiguration=$(shuf -n 1 -e $newWireguardConfigurationList)
+        printf "wg0.conf doesn't exist yet (first time run)\n"
 	
-	sudo mv /etc/wireguard/"$newWireguardConfiguration" /etc/wireguard/wg0.conf
-	echo "$newWireguardConfiguration" > lastconnected
+        # Pick a wireguard interface at random to connect to next
+        newWireguardConfiguration=$(shuf -n 1 -e $newWireguardConfigurationList)
+
+	# Rename randomly selected server to wg0
+        sudo mv /etc/wireguard/"$newWireguardConfiguration" /etc/wireguard/wg0.conf
 	
+	# Write selected server name to lastServer.txt
+	printf "$newWireguardConfiguration" | sudo tee lastServer.txt  # add -a for append (>>)
+
 else
-	echo "wg0.conf already exists"
-	# Pick a wireguard interface dat random to connect to next
-	newWireguardConfiguration=$(shuf -n 1 -e $newWireguardConfigurationList)	
-	lastConnectedInterface=$(<lastconnected)
+        sudo printf "wg0.conf already exists"
 	
-	sudo mv /etc/wireguard/wg0.conf /etc/wireguard/"$lastConnectedInterface".conf
-	sudo mv /etc/wireguard/"$newWireguardConfiguration" /etc/wireguard/wg0.conf
+        # Pick a wireguard interface at random to connect to next
+        newWireguardConfiguration=$(shuf -n 1 -e $newWireguardConfigurationList)
 	
-	echo "$newWireguardConfiguration" > lastconnected
-	    
+	# Read last connected server from lastServer.txt
+        lastConnectedInterface=$(<lastServer.txt)
+
+	# Rename wg0.conf (last connected server) back to actual server name
+        sudo mv /etc/wireguard/wg0.conf /etc/wireguard/"$lastConnectedInterface".conf	
+	# Rename randomly selected server to wg0.conf
+        sudo mv /etc/wireguard/"$newWireguardConfiguration" /etc/wireguard/wg0.conf
+
+	# Write selected server name to lastServer.txt
+	printf "$newWireguardConfiguration" | sudo tee lastServer.txt  # add -a for append (>>)
+
 fi
 
-# lastConnectedInterface=$(<lastconnected)
-# echo "Last connected interface: " $lastConnectedInterface
-# echo "Switching over to $newWireguardConfiguration"
-
-# sudo mv /etc/wireguard/wg0.conf /etc/wireguard/"$lastConnectedInterface".conf
-# sudo mv /etc/wireguard/"$newWireguardConfiguration" /etc/wireguard/wg0.conf
-
-echo "Disconnecting from WireGuard (if tunnel was up"
+printf "Disconnecting from WireGuard (if tunnel was up)\n"
 sudo wg-quick down wg0 #2> /dev/null
-echo "Reconnecting to new WireGuard server"
+printf "Reconnecting to new WireGuard server\n"
 sudo wg-quick up wg0 #2> /dev/null
